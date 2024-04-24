b36b8879d116ce171043aca437943ddfb099e4a0
Utsav <utsavtiwari030@outlook.com>
2024-04-24 09:13:29 +0530

    first commit

--- conn/conn.js
+++ conn/conn.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
 
 const conn = async (req, res) => {
-  try {
-    await mongoose
-      .connect(
-        ""
-      )
-      .then(() => {
-        console.log("Connected");
-      });
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await mongoose.connect("mongodb+srv://todominato:todominato@todo.cgd6jbr.mongodb.net/");
+        console.log("database connected");
+    } catch (error) {
+        // Since 'res' is not available here, you might want to throw the error or handle it differently.
+        console.error("Database connection error:", error);
+    }
 };
+
+
+
 conn();
